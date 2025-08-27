bf8165ba3c41d71d3e59df83f006dd61c00edd6a
marcosifms <marcos.vilhanueva@ifms.edu.br>
2025-08-27 17:25:24 -0400

    Finalizei os testes do habilitar e criei o limpar.

--- src/tools/Util.java
+++ src/tools/Util.java
@@ -5,16 +5,23 @@
 package tools;
 
 import javax.swing.JComponent;
+import javax.swing.JTextField;
 
 /**
  *
  * @author u1845853
  */
 public class Util {
+    
     public static void habilitar(boolean valor, JComponent ... componentes) {
         for (int i = 0; i < componentes.length; i++) {
-            componentes[i].setEnabled(valor);
-            
+            componentes[i].setEnabled(valor);            
         }
+    }
+    
+    public static void limpar(JComponent ... componentes){
+        for (int i = 0; i < componentes.length; i++) {
+            ((JTextField)componentes[i]).setText("");
+        }  
     }
 }
